f5ac3ae3302f024faa076f1d0bbfdd036fd90b61
Bravenorth <hubert.aurelien91@gmail.com>
2025-05-21 09:47:42 +0200

    version handler

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { readFileSync } from 'fs';
+
+// ⬇️ Récupération dynamique de la version du package.json
+const { version } = JSON.parse(readFileSync('./package.json', 'utf-8'));
 
 export default defineConfig({
   plugins: [react()],
   base: '/Augment/',
+  define: {
+    __APP_VERSION__: JSON.stringify(version), // ⬅️ Variable globale disponible dans ton app
+  },
   server: {
     proxy: {
       '/api': {
